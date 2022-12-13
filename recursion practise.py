b3030199902707dd6853f7b90ff8d275a688e8a4
Harshit Kumar <115911046+Harshitkumar05@users.noreply.github.com>
2022-12-13 21:56:34 +0530

    Add files via upload

---
+++ recursion practise.py
@@ -0,0 +1,25 @@
+'''def morning():
+    print("Good morning brother")
+    morning()
+morning()'''
+
+
+'''import sys
+
+sys.setrecursionlimit(100)
+def bro():
+    print("my friend is my son")
+    bro()
+bro()'''
+'''def factor(n):
+    if n == 0:
+        return 1
+    return n*factor(n-1)
+num = factor(5)
+print(num)'''
+
+'''def sqr(p):
+    return p**2
+num = sqr(25)
+print(num)'''
+
